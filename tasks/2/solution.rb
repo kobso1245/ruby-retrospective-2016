f419557f46c40f8b6ebd65453381c4d3d9330911
kobso1245 <kalo.evt@mail.bg>
2017-01-15 15:06:19 +0200

    Adding fixes on taks 2

--- tasks/2/solution.rb
+++ tasks/2/solution.rb
@@ -1,7 +1,7 @@
 class Hash
   def convert
     {
-      Hash => -> (key, el) { [key.to_sym, key].select { |x| el.key? x }[0] },
+      Hash => -> (key, val) { ([key.to_sym, key] & val.keys).first },
       Array => -> (key, _) { key.to_i if key.to_i.to_s == key },
       nil => nil
     }
@@ -9,8 +9,8 @@ class Hash
 
   def key_value
     {
-      Hash => -> (strct) { strct.map { |x, y| [x, y] } },
-      Array => -> (strct) { strct.map.with_index { |x, i| [i, x] } },
+      Hash => -> (strct) { strct.flatten },
+      Array => -> (strct) { strct.map.with_index(&:reverse) },
       nil => []
     }
   end
@@ -22,34 +22,33 @@ class Hash
 
   def fetch_rec(path, strct)
     key = path.shift
-    strct_type = [Hash, Array].select { |n| strct.is_a? n }[0]
+    strct_type = ([Hash, Array].include? strct.class) ? strct.class : nil
     return nil unless strct_type
     return strct[convert[strct_type].call(key, strct)] if path.empty?
     fetch_rec(path, strct[convert[strct_type].call(key, strct)])
   end
 
   def reshape(shape)
-    type = Hash
     new_h = {}
-    elems = key_value[type].call(shape)
-    elems.each { |x, _| reshape_rec(x, shape, self, new_h) }
+    elems = key_value[Hash].call(shape)
+    elems.each { |key, _| reshape_rec(key, shape, self, new_h) }
     new_h
   end
 
-  def reshape_rec(key, shape, order, new_h)
-    type = [Hash, Array, String].select { |n| shape[key].is_a? n }[0]
+  def reshape_rec(key, sh, order, new_h)
+    type = ([Hash, Array, String].include? sh[key].class) ? sh[key].class : nil
     if type == String
-      new_h[key] = order.fetch_deep(shape[key])
+      new_h[key] = order.fetch_deep(sh[key])
     elsif type
-      elems = key_value[type].call(shape[key])
-      new_h[key] = shape[key].dup
-      elems.each { |x, _| reshape_rec(x, shape[key], order, new_h[key]) }
+      elems = key_value[type].call(sh[key])
+      new_h[key] = sh[key].dup
+      elems.each { |new_k, _| reshape_rec(new_k, sh[key], order, new_h[key]) }
     end
   end
 end
 
 class Array
   def reshape(shape)
-    map { |x| x.reshape(shape) }
+    map { |value| value.reshape(shape) }
   end
 end
